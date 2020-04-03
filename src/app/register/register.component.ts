ccd91eeac2cdefdea689f612e3a63790a9228924
yesica427 <60457964+yesica427@users.noreply.github.com>
2020-04-02 20:41:29 -0600

    validacion login y registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup,Validators } from '@angular/forms'
+ import { FormControl,FormGroup,Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -8,31 +8,115 @@ import { FormControl,FormGroup,Validators } from '@angular/forms'
 })
 export class RegisterComponent implements OnInit {
 
-  formularioRegistro = new FormGroup({
-    primerNombre:new FormControl('Yesica'),
-    segundoNombre:new FormControl('Maryori'),
-    PrimerApellido:new FormControl('rodriguez'),
-    segundoApellido:new FormControl('escalante'),
-    contrasena1:new FormControl('',[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,12}$/)]),
-    contrasena2: new FormControl(''),
-    identidad: new FormControl(''),
-    correo: new FormControl('',[Validators.required,Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)])
+   formularioRegistro = new FormGroup({
+     primerNombre:new FormControl('Yesica',[Validators.required,Validators.pattern(/^[A-Za-z _]*[A-Za-z][A-Za-z _]*$/ ),Validators.minLength(3)]),
+     segundoNombre:new FormControl('Maryori',[Validators.required,Validators.pattern(/^[A-Za-z _]*[A-Za-z][A-Za-z _]*$/ ),Validators.minLength(3)]),
+     PrimerApellido:new FormControl('rodriguez',[Validators.required,Validators.pattern(/^[A-Za-z _]*[A-Za-z][A-Za-z _]*$/ ),Validators.minLength(5)]),
+     segundoApellido:new FormControl('escalante',[Validators.required,Validators.pattern(/^[A-Za-z _]*[A-Za-z][A-Za-z _]*$/ ),Validators.minLength(5)]),
+     contrasena1:new FormControl('',[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,12}$/)]),
+     contrasena2: new FormControl('',[Validators.required]),
+     identidad: new FormControl('',[Validators.required,Validators.minLength(13)]),
+     correo: new FormControl('',[Validators.required,Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+    
 
 
-  });
+   });
 
 
 
 
+
+
+
+   
 
   constructor() { }
 
   ngOnInit(): void {
   }
-guardar(){
+
+
+  get primerNombre(){
+    return this.formularioRegistro.get('primerNombre')
+  }
+
+  get segundoNombre(){
+    return this.formularioRegistro.get('segundoNombre')
+  }
+
+
+
+  get PrimerApellido(){
+    return this.formularioRegistro.get('PrimerApellido')
+  }
+
+  get segundoApellido(){
+    return this.formularioRegistro.get('segundoApellido')
+  }
+
+
+  get contrasena1(){
+    return this.formularioRegistro.get('contrasena1')
+  }
+
+
+
+  get contrasena2(){
+    return this.formularioRegistro.get('contrasena2')
+  }
+
+  get identidad(){
+    return this.formularioRegistro.get('identidad')
+  }
+
+
+  get correo(){
+    return this.formularioRegistro.get('correo')
+  }
+
+
+
+contrasenavalida(){
+
+
+  if (this.formularioRegistro.get('contrasena1').value ==this.formularioRegistro.get('contrasena2').value ){
+   var  bool=true;
+   console.log(bool);
   
+   return bool;
+
+
+    
+  }
+
+ else{
+
+  bool=false;
+  return bool
+   
+    
+
+
+
+ }
 }
 
 
 
+
+
+
+
+guardar(){
+   console.log(this.formularioRegistro.value);
+   console.log('iniciovalido:' ,this.formularioRegistro.valid)
+
+
+   
+
 }
+
+
+}
+
+
