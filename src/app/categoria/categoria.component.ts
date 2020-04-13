5b1c50f79b765b3ff513762a3ba856f3ad87a07e
yesica427 <60457964+yesica427@users.noreply.github.com>
2020-04-13 15:42:48 -0600

    categoria eliminar y obtener

--- src/app/categoria/categoria.component.ts
+++ src/app/categoria/categoria.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Categorias} from '../categorias.model';
+import { HttpClient } from '@angular/common/http';
+import {FormControl,FormGroup,Validators} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-categoria',
@@ -7,9 +12,117 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public listaCategorias:Categorias[];
 
   ngOnInit() {
+
+    
+    this.traerCategorias();
+  }
+
+
+  traerCategorias(){
+
+    this.http.get<Categorias[]>('http://localhost:8888/categorias').subscribe(
+      (res)=>{console.log(res)
+      this.listaCategorias=res;
+      }
+    )
+  
+  }
+
+
+
+
+
+  eliminarCategorias(idCategoria:string){
+    this.http.delete('http://localhost:8888/categorias/'+idCategoria).subscribe(
+      (res)=>{console.log(res)
+  
+        this.traerCategorias();
+      }
+    );
+  }
+
+
+
+
+  categoriasForm = new FormGroup({
+    nombreCategorias: new FormControl("", [
+      Validators.required,
+      Validators.minLength(5),
+      
+    ]),
+
+
+    descripcion:new FormControl("",[Validators.required,Validators.minLength(5)]),
+  });
+
+
+  get nombreCategorias() {
+    return this.categoriasForm.get("nombreCategorias");
+  }
+
+
+
+  get descripcion(){
+    return this.categoriasForm.get("descripcion");
+  }
+
+
+    /*editarcategoria*/
+
+     public categoriaEditar:Categorias;
+
+
+     editarCategorias(categorias:Categorias){
+       this.categoriaEditar=categorias;
+       this.categoriasForm.get('nombreCategorias').setValue(categorias.nombrecategoria);
+       this.categoriasForm.get('descripcion').setValue(categorias.descripcion);
+      
+    
+     }
+
+
+
+
+
+
+
+
+/*gurdar categorias*/
+
+guardarCategorias(){
+
+var nombrecategoria = this.categoriasForm.value.nombreCategorias;
+var descripcioncategoria= this.categoriasForm.value.descripcion;
+
+
+
+
+var nuevaCategoria = new Categorias();
+nuevaCategoria.nombrecategoria = nombrecategoria;
+nuevaCategoria.descripcion= descripcioncategoria;
+
+
+  this.http.put('http://localhost:8888/categorias/'+ this.categoriaEditar._id ,nuevaCategoria).subscribe(
+    (res)=>{console.log(res)
+    this.traerCategorias() 
+  }
+  )
+
+
+
+  
+
+  //TODO: Hacer el llamado a  la base y mandar el post
+
+
+  console.log(nuevaCategoria);
+
+
   }
 
 }
---
+++ src/app/categorias.model.ts
@@ -0,0 +1,10 @@
+export class Categorias{
+
+
+_id:string;
+nombrecategoria:string;
+descripcion:string;
+
+
+
+}
