84f97c87e829c828889c5c483cfe55b68109816f
yesica427 <60457964+yesica427@users.noreply.github.com>
2020-04-14 14:49:48 -0600

    modal archivos

--- src/app/archivos/archivos.component.ts
+++ src/app/archivos/archivos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-archivos',
@@ -6,10 +8,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./archivos.component.css']
 })
 export class ArchivosComponent implements OnInit {
+  display='none';
 
   constructor() { }
+  close(){
+    this.display='none';
+  }
 
   ngOnInit() {
+
+    
+  }
+
+
+  openModal(){
+    this.display="block";
+  }
+
+  
+
+
+  archivosForm = new FormGroup({
+    url: new FormControl("", [
+      Validators.required,
+     
+
+    ]),
+
+
+    titulo: new FormControl("", [Validators.required, Validators.minLength(5)]),
+    categoria: new FormControl("", [Validators.required, Validators.minLength(5)]),
+    descripcion: new FormControl("", [Validators.required, Validators.minLength(5)]),
+    urlcaratula:new FormControl("", [Validators.required ]),
+    tipo:new FormControl("", [Validators.required, Validators.minLength(5)]),
+    shorcut:new FormControl("", [Validators.required, Validators.minLength(5)]),
+  });
+
+
+  get titulo() {
+    return this.archivosForm.get("titulo");
+  }
+
+
+
+  get categoria() {
+    return this.archivosForm.get("categoria");
+  }
+
+
+  get descripcion() {
+    return this.archivosForm.get("descripcion");
+  }
+
+
+
+  get urlcaratula() {
+    return this.archivosForm.get("urlcaratula");
+  }
+  
+
+
+  get tipo() {
+    return this.archivosForm.get("tipo");
+  }
+  
+
+  get shorcut() {
+    return this.archivosForm.get("shorcut");
   }
 
 }
