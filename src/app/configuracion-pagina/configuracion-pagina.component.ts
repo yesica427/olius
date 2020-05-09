7911f2636e96e741fc68d0d584a851d00621c0a4
yesica427 <60457964+yesica427@users.noreply.github.com>
2020-05-09 15:26:55 -0600

    validacion configuracion pagina

--- src/app/configuracion-pagina/configuracion-pagina.component.ts
+++ src/app/configuracion-pagina/configuracion-pagina.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl,FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-configuracion-pagina',
   templateUrl: './configuracion-pagina.component.html',
@@ -15,5 +15,47 @@ export class ConfiguracionPaginaComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+
+  formularioConfiguracionPagina = new FormGroup({
+    titulo: new FormControl("", [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+
+    descripcion: new FormControl("", [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+
+    palabrasclave: new FormControl("", [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+  });
+
+
+
+  get titulo() {
+    return this.formularioConfiguracionPagina.get("titulo");
+  }
+
+
+
+  get descripcion() {
+    return this.formularioConfiguracionPagina.get("descripcion");
+  }
+
+  get palabrasclave() {
+    return this.formularioConfiguracionPagina.get("palabrasclave");
+  }
+
+
+
+
+
+
+
 
 }
